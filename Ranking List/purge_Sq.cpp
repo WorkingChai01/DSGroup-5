38c6340684974ef897115acc423bf61a5fc6d6ae
rainy_day <2431006870@qq.com>
2026-03-28 15:34:44 +0800

    Update purge_Sq.cpp

---
+++ Ranking List/purge_Sq.cpp
@@ -0,0 +1,31 @@
+#include "purge_Sq.h"
+#include<string>
+
+void purge_Sq(LeaderBoard* LB) {
+	//为空指针或者元素个数小于等于1时无需去重
+	if (LB == nullptr || LB->size <= 0)
+		return;
+
+	//定义有效边界k,k为下一个元素要插入的位置
+	int k = 0;
+
+	for (int i = 0; i < LB->size; i++) {
+		bool duplicate = false;
+
+		//在已保留的数据中查询
+		for (int j = 0; j < k; j++) {
+			if (LB->elements[i].name == LB->elements[j].name) {
+
+				duplicate = true;
+				break;
+			}
+		}
+		
+		//如果与之前的数据未重复则保留，有效边界后移
+		if (!duplicate) {
+			LB->elements[k] = LB->elements[i];
+			k++;
+		}
+	}
+	LB->size = k;
+}
