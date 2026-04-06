fd5a153a1100b1a60137a408dc71000ccb6f2c6e
daanchm <1609416711@qq.com>
2026-04-06 21:41:47 +0800

    Update SequentialList.h

--- Ranking List/SequentialList.h
+++ Ranking List/SequentialList.h
@@ -4,28 +4,34 @@
 #include <iostream>
 #include <string>
 #include <iomanip>
+#include <ctime> 
 using namespace std;
 
-struct Player{
+struct Player {
     string name;
     int score, subs;
-    Player(string ID = "No data", int sc = 0, int su = 0) : name(ID), score(sc), subs(su) {}
+    time_t timestamp;
+
+    Player(string ID = "No data", int sc = 0, int su = 0, time_t ts = 0)
+        : name(ID), score(sc), subs(su), timestamp(ts) {
+    }
 };
 
-struct LeaderBoard{
-    Player *elements;
+struct LeaderBoard {
+    Player* elements;
     int size;
     int capacity;
 };
 
-void InitBoard(LeaderBoard *LB, int capacity);
-void Destroy(LeaderBoard *LB);
+void InitBoard(LeaderBoard* LB, int capacity);
+void Destroy(LeaderBoard* LB);
 //查询大小
-int Size(LeaderBoard *LB);
+int Size(LeaderBoard* LB);
 //判断是否为空顺序表
-bool IsEmpty(LeaderBoard *LB);
-//插入
-void Insert(LeaderBoard *LB, int index, Player element);
+bool IsEmpty(LeaderBoard* LB);
+//输出
+void Insert(LeaderBoard* LB, int index, Player element);
 //输出
 void PrintBoard(const LeaderBoard* LB);
+
 #endif
